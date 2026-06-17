c2adf8c4ee35fd062abb0b1ebfaa6f0632a3da32
macagomez11 <macarenagomez5167@gmail.com>
2026-06-17 16:11:07 -0300

    Mi diseño Prime System

---
+++ bookflow/PrimeSystem_2026/PrimeSystem_2026/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace PrimeSystem_2026
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelContenido = new System.Windows.Forms.Panel();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.btn_desconectar = new System.Windows.Forms.Button();
+            this.btn_proveedores = new System.Windows.Forms.Button();
+            this.btn_estadocontable = new System.Windows.Forms.Button();
+            this.btn_usuario = new System.Windows.Forms.Button();
+            this.btn_ventas = new System.Windows.Forms.Button();
+            this.btn_clientes = new System.Windows.Forms.Button();
+            this.btn_articulos = new System.Windows.Forms.Button();
+            this.btn_rrhh = new System.Windows.Forms.Button();
+            this.btn_inicio = new System.Windows.Forms.Button();
+            this.panelMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelContenido
+            // 
+            this.panelContenido.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.panelContenido.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelContenido.BackgroundImage")));
+            this.panelContenido.Location = new System.Drawing.Point(200, 0);
+            this.panelContenido.Name = "panelContenido";
+            this.panelContenido.Size = new System.Drawing.Size(764, 523);
+            this.panelContenido.TabIndex = 0;
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panelMenu.Controls.Add(this.btn_desconectar);
+            this.panelMenu.Controls.Add(this.btn_proveedores);
+            this.panelMenu.Controls.Add(this.btn_estadocontable);
+            this.panelMenu.Controls.Add(this.btn_usuario);
+            this.panelMenu.Controls.Add(this.btn_ventas);
+            this.panelMenu.Controls.Add(this.btn_clientes);
+            this.panelMenu.Controls.Add(this.btn_articulos);
+            this.panelMenu.Controls.Add(this.btn_rrhh);
+            this.panelMenu.Controls.Add(this.btn_inicio);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(203, 498);
+            this.panelMenu.TabIndex = 1;
+            // 
+            // btn_desconectar
+            // 
+            this.btn_desconectar.BackColor = System.Drawing.Color.Transparent;
+            this.btn_desconectar.FlatAppearance.BorderSize = 0;
+            this.btn_desconectar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_desconectar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_desconectar.Location = new System.Drawing.Point(36, 383);
+            this.btn_desconectar.Name = "btn_desconectar";
+            this.btn_desconectar.Size = new System.Drawing.Size(132, 30);
+            this.btn_desconectar.TabIndex = 8;
+            this.btn_desconectar.Text = "Desconectar";
+            this.btn_desconectar.UseVisualStyleBackColor = false;
+            // 
+            // btn_proveedores
+            // 
+            this.btn_proveedores.BackColor = System.Drawing.Color.Transparent;
+            this.btn_proveedores.FlatAppearance.BorderSize = 0;
+            this.btn_proveedores.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_proveedores.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_proveedores.Location = new System.Drawing.Point(40, 304);
+            this.btn_proveedores.Name = "btn_proveedores";
+            this.btn_proveedores.Size = new System.Drawing.Size(130, 30);
+            this.btn_proveedores.TabIndex = 1;
+            this.btn_proveedores.Text = "Proveedores";
+            this.btn_proveedores.UseVisualStyleBackColor = false;
+            this.btn_proveedores.Click += new System.EventHandler(this.btn_proveedores_Click);
+            // 
+            // btn_estadocontable
+            // 
+            this.btn_estadocontable.BackColor = System.Drawing.Color.Transparent;
+            this.btn_estadocontable.FlatAppearance.BorderSize = 0;
+            this.btn_estadocontable.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_estadocontable.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_estadocontable.Location = new System.Drawing.Point(28, 343);
+            this.btn_estadocontable.Name = "btn_estadocontable";
+            this.btn_estadocontable.Size = new System.Drawing.Size(155, 30);
+            this.btn_estadocontable.TabIndex = 7;
+            this.btn_estadocontable.Text = "Estado Contable";
+            this.btn_estadocontable.UseVisualStyleBackColor = false;
+            this.btn_estadocontable.Click += new System.EventHandler(this.btn_estadocontable_Click);
+            // 
+            // btn_usuario
+            // 
+            this.btn_usuario.BackColor = System.Drawing.Color.Transparent;
+            this.btn_usuario.FlatAppearance.BorderSize = 0;
+            this.btn_usuario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_usuario.Location = new System.Drawing.Point(52, 154);
+            this.btn_usuario.Name = "btn_usuario";
+            this.btn_usuario.Size = new System.Drawing.Size(102, 30);
+            this.btn_usuario.TabIndex = 1;
+            this.btn_usuario.Text = "Usuario";
+            this.btn_usuario.UseVisualStyleBackColor = false;
+            // 
+            // btn_ventas
+            // 
+            this.btn_ventas.BackColor = System.Drawing.Color.Transparent;
+            this.btn_ventas.FlatAppearance.BorderSize = 0;
+            this.btn_ventas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_ventas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ventas.Location = new System.Drawing.Point(64, 193);
+            this.btn_ventas.Name = "btn_ventas";
+            this.btn_ventas.Size = new System.Drawing.Size(75, 30);
+            this.btn_ventas.TabIndex = 2;
+            this.btn_ventas.Text = "Ventas";
+            this.btn_ventas.UseVisualStyleBackColor = false;
+            // 
+            // btn_clientes
+            // 
+            this.btn_clientes.BackColor = System.Drawing.Color.Transparent;
+            this.btn_clientes.FlatAppearance.BorderSize = 0;
+            this.btn_clientes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_clientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_clientes.Location = new System.Drawing.Point(61, 118);
+            this.btn_clientes.Name = "btn_clientes";
+            this.btn_clientes.Size = new System.Drawing.Size(84, 30);
+            this.btn_clientes.TabIndex = 4;
+            this.btn_clientes.Text = "Clientes";
+            this.btn_clientes.UseVisualStyleBackColor = false;
+            // 
+            // btn_articulos
+            // 
+            this.btn_articulos.BackColor = System.Drawing.Color.Transparent;
+            this.btn_articulos.FlatAppearance.BorderSize = 0;
+            this.btn_articulos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_articulos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_articulos.Location = new System.Drawing.Point(52, 230);
+            this.btn_articulos.Name = "btn_articulos";
+            this.btn_articulos.Size = new System.Drawing.Size(102, 30);
+            this.btn_articulos.TabIndex = 5;
+            this.btn_articulos.Text = "Libros";
+            this.btn_articulos.UseVisualStyleBackColor = false;
+            this.btn_articulos.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // btn_rrhh
+            // 
+            this.btn_rrhh.BackColor = System.Drawing.Color.Transparent;
+            this.btn_rrhh.FlatAppearance.BorderSize = 0;
+            this.btn_rrhh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_rrhh.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_rrhh.Location = new System.Drawing.Point(64, 268);
+            this.btn_rrhh.Name = "btn_rrhh";
+            this.btn_rrhh.Size = new System.Drawing.Size(75, 30);
+            this.btn_rrhh.TabIndex = 6;
+            this.btn_rrhh.Text = "RRHH";
+            this.btn_rrhh.UseVisualStyleBackColor = false;
+            this.btn_rrhh.Click += new System.EventHandler(this.btn_rrhh_Click);
+            // 
+            // btn_inicio
+            // 
+            this.btn_inicio.BackColor = System.Drawing.Color.Transparent;
+            this.btn_inicio.FlatAppearance.BorderSize = 0;
+            this.btn_inicio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_inicio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_inicio.Location = new System.Drawing.Point(64, 82);
+            this.btn_inicio.Name = "btn_inicio";
+            this.btn_inicio.Size = new System.Drawing.Size(75, 30);
+            this.btn_inicio.TabIndex = 0;
+            this.btn_inicio.Text = "Inicio";
+            this.btn_inicio.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 498);
+            this.Controls.Add(this.panelMenu);
+            this.Controls.Add(this.panelContenido);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panelMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelContenido;
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Button btn_desconectar;
+        private System.Windows.Forms.Button btn_proveedores;
+        private System.Windows.Forms.Button btn_estadocontable;
+        private System.Windows.Forms.Button btn_usuario;
+        private System.Windows.Forms.Button btn_ventas;
+        private System.Windows.Forms.Button btn_clientes;
+        private System.Windows.Forms.Button btn_articulos;
+        private System.Windows.Forms.Button btn_rrhh;
+        private System.Windows.Forms.Button btn_inicio;
+    }
+}
+
